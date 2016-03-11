238215f8275c143177b62238a8d51cf1bceeca7b
Gideon Wenniger <gideonwenniger@domain.com>
2016-03-11 13:14:24 +0100

    Added a new class LabelSubstitutionFFValuePrediction.java.     With this class the scores for LabelSubstitution features     will be predictable beforehand, which makes it possible     to predict the best possible score for the hypothetically     best labeled variant of a rule that is substituted beforehand.
        This can then be used to significantly optimize the decoding
        process when doing fuzzy matching, by directly rejecting a
        whole set of rule applications that only differ in the
        labels of the substituted to gaps when the best of these
        labeled variants is worse than the POP-LIMIT-best
        (e.g. 1000-best) candidate in the PriorityQueue.
    
        Notice that apart from finishing this implementation, as a
        next step we probably also have to replace PriorityQueue
        that is used during cube pruning
        with a priorityqueue with limited capacity.
        This is needed to allow to find he
        POP-LIMIT-best candidate in constant time. The class
        "MinMaxPriorityQueue" from the guave library allows
        to do this, and is therefore a good candidate
    
    	new file:   src/joshua/decoder/ff/LabelSubstitutionFFValuePrediction.java

---
+++ src/joshua/decoder/ff/LabelSubstitutionFFValuePrediction.java
@@ -0,0 +1,108 @@
+package joshua.decoder.ff;
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * This class stores predictions for LabelSubstitionFeatures for all possible <rule labels, gap
+ * labels> combinations that are possible, given the nonterminal labels existing in the grammar.
+ * 
+ * @author gemaille
+ * 
+ */
+public class LabelSubstitutionFFValuePrediction {
+
+  // A map that stores the mapping from specific label substitution combinations to lists of feature
+  // names
+  // private final Map<LabelSubstitutionCombination, List<String>>
+  // labelSubstitutionCombinationToFeaturesMap;
+
+  // This map stores the mapping from LabelSubstitionCombination to score.
+  // The score depends on the feature weights, therefore, this map needs to be updated whenever the
+  // feature
+  // weights are changed (after every iteration during normal tuning with MIRA).
+  // However, this is not a problem, as the decoder is called freshly for every iteration during
+  // tuning
+  private final Map<LabelSubstitutionCombination, Double> labelSubstitutionCombinationToScoresMap;
+
+  private LabelSubstitutionFFValuePrediction(
+      Map<LabelSubstitutionCombination, Double> labelSubstitutionCombinationToScoresMap) {
+    this.labelSubstitutionCombinationToScoresMap = labelSubstitutionCombinationToScoresMap;
+
+  }
+
+  public static LabelSubstitutionFFValuePrediction createLabelSubstitutionFFValuePrediction() {
+    return new LabelSubstitutionFFValuePrediction(computeLabelSubstitutionCombinationScoresMap());
+  }
+
+  public static Map<LabelSubstitutionCombination, Double> computeLabelSubstitutionCombinationScoresMap() {
+    Map<LabelSubstitutionCombination, Double> result = new HashMap<LabelSubstitutionFFValuePrediction.LabelSubstitutionCombination, Double>();
+
+    // TODO implement:
+    // 1: Loop over all possible LabelSubstitution Combinations
+
+    // 2: For each LabelSubstitutionCombination, compute list of produced features (Strings)
+
+    // 3. For each produced feature list (= list of strings) compute the resulting feature score
+
+    // 4. Add the mapping <LabelSubstitutionCombination,score> to the result map
+
+    return result;
+  }
+
+  /**
+   * A class for characterizing a particular label substitution, which is described by only the rule
+   * source nonterminals and the substitution nonterminals (labels of the gaps)
+   * 
+   * @author gemaille
+   * 
+   */
+  private static class LabelSubstitutionCombination {
+    final List<String> ruleSourceNonterminals;
+    final List<String> substitutionNonterminals;
+
+    private LabelSubstitutionCombination(List<String> ruleSourceNonterminals,
+        List<String> substitutionNonterminals) {
+      this.ruleSourceNonterminals = ruleSourceNonterminals;
+      this.substitutionNonterminals = substitutionNonterminals;
+    }
+  }
+
+  /**
+   * This class will enumerate all LabelSubstitutionCombinations
+   * 
+   * @author gemaille
+   * 
+   */
+  public static class LabelSubstituionCombinationEnumeration implements
+      Enumeration<LabelSubstitutionCombination> {
+
+    private final int numberOfLabels;
+    private List<Integer> nextElementRuleSourceNonterminalNumbers;
+    private List<Integer> nextElementSubstitutedNonterminalNumbers;
+
+    private LabelSubstituionCombinationEnumeration(int numberOfLabels,
+        List<Integer> nextElementRuleSourceNonterminalNumbers,
+        List<Integer> nextElementSubstitutedNonterminalNumbers) {
+      this.numberOfLabels = numberOfLabels;
+      this.nextElementSubstitutedNonterminalNumbers = nextElementSubstitutedNonterminalNumbers;
+      this.nextElementRuleSourceNonterminalNumbers = nextElementRuleSourceNonterminalNumbers;
+    }
+
+    @Override
+    public boolean hasMoreElements() {
+      // TODO Auto-generated method stub
+      return false;
+    }
+
+    @Override
+    public LabelSubstitutionCombination nextElement() {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
+  }
+
+}
